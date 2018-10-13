67c191d99d1c17cc02117ba28e8187e52d742673
lazybear <jasonwhister@gmail.com>
2018-10-13 19:40:26 +0800

    Update follow and unfollow model/route

--- models/follow.py
+++ models/follow.py
@@ -12,17 +12,11 @@ class Follow(Mongoo):
         ]
         return names
 
-    def _get_follower_id(self):
-        return self.follower_id
-
     def _get_following_id(self):
         return self.following_id
 
-    @staticmethod
-    def _add_follower(user_id, other_id):
-        f = Follow.find_by(user_id=user_id)
-        f._get_follower_id().append(other_id)
-        f.save()
+    def _get_follower_id(self):
+        return self.follower_id
 
     @staticmethod
     def _add_following(user_id, other_id):
@@ -31,6 +25,29 @@ class Follow(Mongoo):
         f.save()
 
     @staticmethod
+    def _add_follower(user_id, other_id):
+        f = Follow.find_by(user_id=user_id)
+        f._get_follower_id().append(other_id)
+        f.save()
+
+    @staticmethod
+    def _delete_following(user_id, other_id):
+        f = Follow.find_by(user_id=user_id)
+        f._get_following_id().remove(other_id)
+        f.save()
+
+    @staticmethod
+    def _delete_follower(user_id, other_id):
+        f = Follow.find_by(user_id=user_id)
+        f._get_following_id().remove(other_id)
+        f.save()
+
+    @staticmethod
     def add_follow(user_id, other_id):
+        Follow._add_following(user_id, other_id)
+        Follow._add_follower(other_id, user_id)
+
+    @staticmethod
+    def delete_follow(user_id, other_id):
         Follow._add_following(user_id, other_id)
         Follow._add_follower(other_id, user_id)
--- routes/user.py
+++ routes/user.py
@@ -13,6 +13,7 @@ main = Blueprint('user', __name__)
 
 @main.route("/<string:username>")
 def user(username):
+    u = current_user()
     user = User.find_by(username=username)
     if user is not None:
         topic = sort_by_create_time(Topic.find_all(user_id=user.id))
@@ -23,11 +24,15 @@ def user(username):
         sorted_topic = [Topic.find_by(id=id) for id in sorted_topic_id]
 
         f = Follow.find_by(user_id=user.id)
-        following = [User.find_by(id=id) for id in f.following_id]
-        follower = [User.find_by(id=id) for id in f.follower_id]
+        following = [User.find_by(id=id) for id in set(f.following_id)]
+        follower = [User.find_by(id=id) for id in set(f.follower_id)]
+
+        button_id = "follow"
+        if u.id in f.follower_id:
+            button_id = "unfollow"
 
         return render_template("user.html", user=user, ts=topic, ots=sorted_topic, following=following,
-                               follower=follower)
+                               follower=follower, button_id=button_id)
     else:
         abort(403)
 
@@ -37,7 +42,17 @@ def user(username):
 def follow(username):
     u = current_user()
     user = User.find_by(username=username)
-    if user is not None:
+    if user is not None and u != user:
         u_id = user.id
         Follow.add_follow(u.id, u_id)
+    return None
+
+
+@main.route("/<string:username>/unfollow")
+def unfollow(username):
+    u = current_user()
+    user = User.find_by(username=username)
+    if user is not None and u != user:
+        u_id = user.id
+        Follow.delete_follow(u.id, u_id)
     return None
